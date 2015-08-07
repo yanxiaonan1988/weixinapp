e2b3540eda54ed7e66c5dc7619a27672d62a8a64
yanxiaonan1988 <yanxiaonan1988@gmail.com>
2015-08-07 18:21:56 +0800

    hecheng config

--- src/main/java/hermione/business/HcaxSubscribeEventMessageBusiness.java
+++ src/main/java/hermione/business/HcaxSubscribeEventMessageBusiness.java
@@ -16,7 +16,9 @@ public class HcaxSubscribeEventMessageBusiness {
 		res.setFromUserName(req.getToUserName());
 		System.out.println("openid:"+res.getToUserName());
 		res.setCreateTime(new Date().getTime());
-		res.setContent("您好，感谢您关注我们的和诚安心保险销售有限公司官方微信平台！全国客服热线：4009267777从这一刻起，我们将一直陪伴和关注您的保险情况。了解更多和诚安心保险销售有限公司请回复1；和诚安心保险产品推荐请回复2；和诚安心保险人工服务请回复3；和诚汽车爱车小贴士请回复4； 轻松一刻请回复5.");
+		res.setContent("您好，感谢您关注我们的和诚安心保险销售有限公司官方微信平台！\r\n" +
+				"全国客服热线：0571-86578905\r\n" +
+				"从这一刻起，我们将一直陪伴和关注您的保险情况。");
 		
 		return res;	
 		
