d1be5487455afdb2a1496b14bc4329f72dc39d6c
aaron81939097 <81939097@qq.com>
2015-08-25 17:57:31 +0800

    安心保关注事件

--- src/main/java/hermione/business/HcaxSubscribeEventMessageBusiness.java
+++ src/main/java/hermione/business/HcaxSubscribeEventMessageBusiness.java
@@ -1,16 +1,48 @@
 package hermione.business;
 
 import hermione.model.message.EventMessageRequest;
-import hermione.model.message.Item;
 import hermione.model.message.TextMessageResponse;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Date;
-import java.util.List;
 
 public class HcaxSubscribeEventMessageBusiness {
 	public TextMessageResponse process(EventMessageRequest req){
 		System.out.println("####HcaxSubscribeEventMessageBusiness");
+		
+		String openid = req.getFromUserName();
+		String sceneid = null;
+		if(req.getEvent().equals("subscribe")){
+			if(req.getEventKey()!=null && !req.getEventKey().equals("")){
+				sceneid = req.getEventKey().substring(8, req.getEventKey().length());
+				System.out.println("newscan:"+sceneid);
+			}
+		}
+		if(req.getEvent().equals("SCAN")){
+			if(req.getEventKey()!=null && !req.getEventKey().equals("")){
+				sceneid = req.getEventKey();
+				System.out.println("oldscan:"+sceneid);
+			}
+		}
+		
+		URL url;
+		try {
+			url = new URL("http://wx.99bx.cn/api/hcax/wxReferee?openId="+openid+"&sceneId="+sceneid);
+			URLConnection conn = url.openConnection();
+			InputStream in = conn.getInputStream();
+			in.close();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		TextMessageResponse res = new TextMessageResponse();
 		res.setToUserName(req.getFromUserName());
 		res.setFromUserName(req.getToUserName());
